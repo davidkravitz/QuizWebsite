495b5e9deca80927e22f2d945970b55906d51223
Sheldon Chang <sheldon0789@gmail.com>
2012-03-14 00:56:22 -0700

    started quiz summary servlet

---
+++ src/servlet/QuizSummaryServlet.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Quiz;
+
+/**
+ * Servlet implementation class QuizSummaryServlet
+ */
+@WebServlet("/QuizSummaryServlet")
+public class QuizSummaryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuizSummaryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("In the quiz summary!");
+		
+		String quizName = request.getParameter("quiz");
+		Quiz curr = Quiz.getQuiz(quizName);
+		if(curr != null) {
+			System.out.println("Quiz desc: "+curr.description);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}

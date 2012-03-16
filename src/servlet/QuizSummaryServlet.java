edaad0be36ae5fc6192e646cc6010fd9da1e4dbb
Sheldon Chang <sheldon0789@gmail.com>
2012-03-16 02:37:05 -0700

    quiz summary updated

--- src/servlet/QuizSummaryServlet.java
+++ src/servlet/QuizSummaryServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Quiz;
+import model.User;
 
 /**
  * Servlet implementation class QuizSummaryServlet
@@ -15,6 +16,8 @@ import model.Quiz;
 @WebServlet("/QuizSummaryServlet")
 public class QuizSummaryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String successUrl = "quiz-summary.jsp";
+	private static final String errorUrl = "quiz-summary-error.jsp";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,13 +31,20 @@ public class QuizSummaryServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("In the quiz summary!");
+		int quizId = Integer.valueOf(request.getParameter("quiz"));
+		//Quiz curr = Quiz.getQuiz(quizId);
+		Quiz curr = new Quiz(1, "Math", "Algebra I", "Sheldon", "This quiz is meant for Algebra 1 students", "2012-3-14", false, false, false);
 		
-		String quizName = request.getParameter("quiz");
-		Quiz curr = Quiz.getQuiz(quizName);
+		String username = (String)request.getSession().getAttribute("username");
+		User user = (User) request.getSession().getAttribute("user");
+
 		if(curr != null) {
-			System.out.println("Quiz desc: "+curr.description);
+			//System.out.println("Quiz desc: "+curr.description);
+			request.setAttribute("quiz", curr);
+			request.getRequestDispatcher(successUrl).forward(request, response);
+		}
+		else {
+			request.getRequestDispatcher(errorUrl).forward(request, response);
 		}
 	}
 
